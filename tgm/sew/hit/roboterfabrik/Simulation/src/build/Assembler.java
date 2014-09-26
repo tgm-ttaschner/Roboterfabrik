cada318c5ffcf8b1b4b64b71793df1e027320b42
Alexander Kölbl <alexander.koelbl@student.tgm.ac.at>
2014-09-26 15:44:18 +0200

    +csv-files added +worked on Assembler

--- tgm/sew/hit/roboterfabrik/Simulation/src/build/Assembler.java
+++ tgm/sew/hit/roboterfabrik/Simulation/src/build/Assembler.java
@@ -9,17 +9,27 @@ public class Assembler {
 
 	private IO iO;
 
+	private Assembler a;
+	
 	public Assembler Assembler(int id) {
 		return null;
+		IO i1 = new IO(getAbsolutePath()csv/auge.csv);
 	}
 
 	public void run() {
 		Secretary s = new Secretary();
-		Assembler a = new Assembler(s.getAssemblerID());
+		a = new Assembler(s.getAssemblerID());
 		a.build();
 	}
 
 	public void build(ArrayList<String> parts) {
+		String arm1 = a.deliver(1);
+		String arm2 = a.deliver(1);
+		String torso1 = a.deliver(2);
+		String chaindrive1 = a.deliver(3);
+		String eye1 = a.deliver(4);
+		String eye2 = a.deliver(4);
+		
 		
 	}
 
