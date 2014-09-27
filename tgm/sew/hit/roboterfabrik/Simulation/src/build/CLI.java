4924341bece0c5fec346b7555f0b8dcd45c9f872
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2014-09-27 15:03:20 +0200

    + Added NotEnoughArgumentsException + Added some code in CLI
    - Removed Sync

--- tgm/sew/hit/roboterfabrik/Simulation/src/build/CLI.java
+++ tgm/sew/hit/roboterfabrik/Simulation/src/build/CLI.java
@@ -1,5 +1,71 @@
 package build;
 
+import java.io.*;
+
 public class CLI {
 
+	private String arguments;
+
+	private String lagerVerzeichnis;
+
+	private String logVerzeichnis;
+
+	private int lieferantenAnzahl;
+
+	private int monteurAnzahl;
+
+	private int lagerMitarbeiterAnzahl;
+
+	private int laufzeit;
+
+	private int countargs = 0;
+
+	public CLI(String arguments) throws NotEnoughArgumentsException	{
+
+		this.arguments = arguments;
+
+		if (arguments == null)	{
+			throw new NullPointerException("Ihre Angabe darf nicht null sein!");
+		}
+
+		if (!(arguments.trim()).startsWith("java"))	{
+			throw new IllegalArgumentException("Der Aufruf muss mit 'java' starten!");
+		}
+
+		for (int i = 0; i < arguments.length(); i = i + 4)	{
+			try {
+				if (i+4 < arguments.length())	{
+					if (arguments.substring(i, i+4).contains("--"))	{
+						countargs++;
+					}
+				}
+			} catch (StringIndexOutOfBoundsException e)	{
+				System.out.println("Test");
+			}
+		}
+
+		if (countargs == 0)	{
+			throw new NotEnoughArgumentsException();
+		}
+
+		System.out.println(countargs);
+
+		/*
+		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Geben Sie etwas ein: ");
+		String zeile = "";
+
+		try {
+			zeile = console.readLine();
+		} catch (IOException e) {
+
+		}
+
+		System.out.println("Ihre Eingabe war: " + zeile);
+		 */
+	}
+
+	public static void main(String[] args) throws NotEnoughArgumentsException {
+		new CLI("java tgm.sew.hit.roboterfabrik.Simulation --lager /verzeichnis/zum/lager --logs /verzeichnis/zum/loggen --lieferanten 12 --monteure 25 --laufzeit 10000");
+	}
 }
--- tgm/sew/hit/roboterfabrik/Simulation/src/build/Sync.java
+++
@@ -1,5 +0,0 @@
-package build;
-
-public class Sync {
-
-}
