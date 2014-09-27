90a45b4a3afbc1a65a49d9ffca43254f62e0b73b
mweinberger-tgm <mweinberger@student.tgm.ac.at>
2014-09-27 20:11:09 +0200

    + added tests for IO + added folder, where IO could test-read/write

---
+++ tgm/sew/hit/roboterfabrik/Simulation/src/testing/IOplayground/RecognizeTheDamnPackageGit.java
@@ -0,0 +1,5 @@
+package testing.IOplayground;
+
+public class RecognizeTheDamnPackageGit {
+
+}
---
+++ tgm/sew/hit/roboterfabrik/Simulation/src/testing/TestIO.java
@@ -0,0 +1,17 @@
+package testing;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import build.IO;
+
+/**
+ * 
+ * 
+ * 
+ * @author Michael Weinberger 4AHITT
+ * @version 2014-09-27
+ *
+ */
+public class TestIO {
+
+}
