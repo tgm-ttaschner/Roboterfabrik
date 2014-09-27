465f6fde628d312592c29629f0be0fbdef9fc431
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2014-09-27 20:54:48 +0200

    + Added documentation for TestingUniqueNumbers, Watchable and Watchdog

--- tgm/sew/hit/roboterfabrik/Simulation/src/threading/Watchable.java
+++ tgm/sew/hit/roboterfabrik/Simulation/src/threading/Watchable.java
@@ -1,5 +1,13 @@
 package threading;
 
+/**
+ * This is an interface for the WatchDog.
+ * The only method to be implemented is shutdown().
+ * 
+ * @author Thomas Taschner
+ * @version 27.09.2014
+ *
+ */
 public interface Watchable	{
 	public void shutdown();
 }
--- tgm/sew/hit/roboterfabrik/Simulation/src/threading/Watchdog.java
+++ tgm/sew/hit/roboterfabrik/Simulation/src/threading/Watchdog.java
@@ -2,26 +2,49 @@ package threading;
 
 import java.util.*;
 
+/**
+ * This class implements a WatchDog for threads.
+ * 
+ * @author Thomas Taschner
+ * @version 27.09.2014
+ *
+ */
 public class Watchdog implements Runnable {
 	private int time;
 	private HashSet<Watchable> watchables;
 
 
+	/**
+	 * @param time The time in milliseconds until the WatchDog kills the threads.
+	 */
 	public Watchdog(int time) {
 		this.time = time;
 		this.watchables = new HashSet<Watchable>();
 	}
 
+	
+	/**
+	 * @param w Adds a new thread to the HashSet
+	 */
 	public void add(Watchable w)	{
 		this.watchables.add(w);
 	}
 
+	/**
+	 * Shuts all threads in the HashSet down
+	 */
 	public void killAll()	{
 		for (Watchable watch: watchables) {
 			watch.shutdown();
 		}
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 * 
+	 * Runs the threads and lets them sleep for certain amount of time.
+	 * Finally all threads get killed.
+	 */
 	@Override
 	public void run() {
 		try	{
