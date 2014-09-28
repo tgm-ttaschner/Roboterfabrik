6941d48d1a7079bf618e6181a057a4a3f285c774
mweinberger-tgm <mweinberger@student.tgm.ac.at>
2014-09-28 23:03:18 +0200

    ~ completed furnisher

--- tgm/sew/hit/roboterfabrik/Simulation/src/build/Furnisher.java
+++ tgm/sew/hit/roboterfabrik/Simulation/src/build/Furnisher.java
@@ -12,12 +12,12 @@ import build.IO;
  * @version 2014-09-28
  *
  */
-public class Furnisher {
+public class Furnisher implements Runnable {
 	
 	/**
 	 * This code should be executed by a thread.
 	 */
-	public static void run() {
+	public void run() {
 		IO arm = new IO(IO.getWorkingDir() +CLI.lagerVerzeichnis +"/arm.csv");
 		IO eye = new IO(IO.getWorkingDir() +CLI.lagerVerzeichnis +"/eye.csv");
 		IO torso = new IO(IO.getWorkingDir() +CLI.lagerVerzeichnis +"/torso.csv");
@@ -29,15 +29,19 @@ public class Furnisher {
 		ArrayList<String> alcha = chaindrive.read();
 		
 		int amountarm = alarm.size();
-		int amounteye = alarm.size();
-		int amounttorso = alarm.size();
-		int amountchaindrive = alarm.size();
+		int amounteye = aleye.size();
+		int amounttorso = altor.size();
+		int amountchaindrive = alcha.size();
 		
-		if (amountarm < 210) {
+		System.out.println(amountarm);
+		System.out.println(amounteye);
+		System.out.println(amounttorso);
+		System.out.println(amountchaindrive);
+		
+		if (amountarm < 2100) {
 			arm.overWrite("");
 			for (int i = 0; i < 100; i++) {
-				arm.write("Arm," 
-							+Furnisher.getNumber() +","
+				arm.writeRow("Arm," 
 							+Furnisher.getNumber() +","
 							+Furnisher.getNumber() +","
 							+Furnisher.getNumber() +","
@@ -58,14 +62,14 @@ public class Furnisher {
 							+Furnisher.getNumber() +","
 							+Furnisher.getNumber() +","
 						);
+				arm.write("" +Furnisher.getNumber());
 			}
 		}
 		
-		if (amounteye < 210) {
+		if (amounteye < 2100) {
 			eye.overWrite("");
 			for (int i = 0; i < 100; i++) {
-				eye.write("Eye," 
-							+Furnisher.getNumber() +","
+				eye.writeRow("Eye," 
 							+Furnisher.getNumber() +","
 							+Furnisher.getNumber() +","
 							+Furnisher.getNumber() +","
@@ -86,14 +90,14 @@ public class Furnisher {
 							+Furnisher.getNumber() +","
 							+Furnisher.getNumber() +","
 						);
+				eye.write("" +Furnisher.getNumber());
 			}
 		}
 		
-		if (amounttorso < 210) {
+		if (amounttorso < 2100) {
 			torso.overWrite("");
 			for (int i = 0; i < 100; i++) {
-				torso.write("Torso," 
-							+Furnisher.getNumber() +","
+				torso.writeRow("Torso," 
 							+Furnisher.getNumber() +","
 							+Furnisher.getNumber() +","
 							+Furnisher.getNumber() +","
@@ -114,39 +118,49 @@ public class Furnisher {
 							+Furnisher.getNumber() +","
 							+Furnisher.getNumber() +","
 						);
+				torso.write("" +Furnisher.getNumber());
 			}
 		}
 		
-		if (amountchaindrive < 210) {
+		if (amountchaindrive < 2100) {
 			chaindrive.overWrite("");
-			chaindrive.write("Chaindrive," 
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
-						+Furnisher.getNumber() +","
+			for (int i = 0; i < 100; i++) {
+				chaindrive.writeRow("Chaindrive," 
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
+							+Furnisher.getNumber() +","
 						);
+				chaindrive.write("" +Furnisher.getNumber());
+			}
 		}
 	}
 	
 	public static int getNumber() {
 		Random generator= new Random();
-		return generator.nextInt(100);
+		return generator.nextInt(999);
+	}
+	
+	public static void main(String[] args) {
+		Thread t = new Thread(new Furnisher());
+		CLI.lagerVerzeichnis = "/src/testing/IOplayground/csv";
+		
+		t.start();
 	}
 
 }
--- tgm/sew/hit/roboterfabrik/Simulation/src/testing/TestFurnisher.java
+++ tgm/sew/hit/roboterfabrik/Simulation/src/testing/TestFurnisher.java
@@ -1,5 +1,49 @@
 package testing;
 
-public class TestFurnisher {
+import static org.junit.Assert.*;
 
+import org.junit.*;
+
+import build.CLI;
+import build.Furnisher;
+import build.IO;
+
+/**
+ * 
+ * Checks if the furnisher furnishes properly.
+ * 
+ * @author Michael Weinberger 4AHITT
+ * @version 2014-09-28
+ *
+ */
+public class TestFurnisher {
+	
+	Thread t;
+	
+	@Before
+	public void setup() {
+		CLI.lagerVerzeichnis = "/src/testing/IOplayground/csv";
+		Thread t = new Thread(new Furnisher());
+		
+		IO arm = new IO(IO.getWorkingDir() +CLI.lagerVerzeichnis + "/arm.csv");
+		IO eye = new IO(IO.getWorkingDir() +CLI.lagerVerzeichnis + "/eye.csv");
+		IO torso = new IO(IO.getWorkingDir() +CLI.lagerVerzeichnis + "/torso.csv");
+		IO chaindrive = new IO(IO.getWorkingDir() +CLI.lagerVerzeichnis + "/chaindrive.csv");
+		
+		arm.overWrite("");
+		eye.overWrite("");
+		torso.overWrite("");
+		chaindrive.overWrite("");
+		t.start();
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testfillUp() {
+		
+		IO arm = new IO(IO.getWorkingDir() +CLI.lagerVerzeichnis + "/arm.csv");
+		assertNotNull(arm.read().iterator().next());
+	}
 }
